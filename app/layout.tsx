fa6e91b17c4259377d03c35dc38b10cb17e8e675
Meriç Kalkan <merickalkanp@gmail.com>
2026-01-27 06:02:39 +0300

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css"
 import { Providers } from "./providers"
 import { Toaster } from "@/components/ui/sonner"
 import Footer15 from "@/components/blocks/footer15"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             <Footer15 className="mt-auto" />
           </div>
           <Toaster />
+          <SpeedInsights />
         </Providers>
       </body>
     </html>
