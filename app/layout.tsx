024c7aa67794d75be21dae016fad6a2a50200e79
Meriç Kalkan <merickalkanp@gmail.com>
2026-01-28 23:43:55 +0300

    feat(analytics): integrate yandex metrika

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from "./providers"
 import { Toaster } from "@/components/ui/sonner"
 import Footer15 from "@/components/blocks/footer15"
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -28,6 +29,30 @@ export default function RootLayout({
           </div>
           <Toaster />
           <SpeedInsights />
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106506413', 'ym');
+
+              ym(106506413, 'init', {
+                  ssr: true,
+                  webvisor: true,
+                  clickmap: true,
+                  ecommerce: "dataLayer",
+                  accurateTrackBounce: true,
+                  trackLinks: true
+              });
+            `}
+          </Script>
+          <noscript>
+            <div>
+              <img src="https://mc.yandex.ru/watch/106506413" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+            </div>
+          </noscript>
         </Providers>
       </body>
     </html>
