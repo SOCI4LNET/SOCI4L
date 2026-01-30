9f37c9505097b7612fc7ddcccf1e4daac33bf7fa
Meriç Kalkan <merickalkanp@gmail.com>
2026-01-30 21:00:47 +0300

    fix: Wrap DemoProvider in Suspense to fix build error

--- app/demo/layout.tsx
+++ app/demo/layout.tsx
@@ -1,5 +1,19 @@
+import { Suspense } from 'react'
 import { DemoProvider } from '@/lib/demo/demo-context'
 import { DemoBanner } from '@/components/demo/demo-banner'
+import { Skeleton } from '@/components/ui/skeleton'
+
+function DemoLoading() {
+    return (
+        <div className="min-h-screen flex flex-col">
+            <div className="h-12 border-b bg-background" />
+            <div className="p-8 space-y-6">
+                <Skeleton className="h-32 w-full rounded-xl" />
+                <Skeleton className="h-64 w-full rounded-xl" />
+            </div>
+        </div>
+    )
+}
 
 export default function DemoLayout({
     children,
@@ -7,11 +21,13 @@ export default function DemoLayout({
     children: React.ReactNode
 }) {
     return (
-        <DemoProvider>
-            <div className="relative min-h-screen flex flex-col">
-                <DemoBanner />
-                {children}
-            </div>
-        </DemoProvider>
+        <Suspense fallback={<DemoLoading />}>
+            <DemoProvider>
+                <div className="relative min-h-screen flex flex-col">
+                    <DemoBanner />
+                    {children}
+                </div>
+            </DemoProvider>
+        </Suspense>
     )
 }
