dcce1286f392a32e34e37595bbd4599fc01d492b
Meriç Kalkan <merickalkanp@gmail.com>
2026-02-11 21:56:51 +0300

    fix: allow Yandex Metrica to iframe site for heatmaps/clickmaps

--- next.config.js
+++ next.config.js
@@ -64,8 +64,8 @@ const nextConfig = {
             value: '1; mode=block'
           },
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN'
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://metrika.yandex.ru https://*.metrika.yandex.ru https://metrica.yandex.com https://*.metrica.yandex.com https://metrika.yandex.by https://*.metrika.yandex.by https://metrica.yandex.com.tr https://*.metrica.yandex.com.tr https://webvisor.com https://*.webvisor.com;"
           },
           {
             key: 'X-Content-Type-Options',
