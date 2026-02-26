8629636df01a3742a78c113c0df6bad7e1b8a019
Meriç Kalkan <merickalkanp@gmail.com>
2026-02-26 20:07:47 +0300

    fix: make 'Copy Donate Embed' available to all users instead of just profile owner

--- app/p/[id]/page.tsx
+++ app/p/[id]/page.tsx
@@ -855,12 +855,10 @@ export default function ProfilePage({ params }: PageProps) {
                                                 <Copy className="mr-2 h-4 w-4" />
                                                 Copy Link
                                             </DropdownMenuItem>
-                                            {isOwnProfile && (
-                                                <DropdownMenuItem onClick={handleCopyDonateEmbed} className="hover:bg-foreground/10 focus:bg-foreground/10 cursor-pointer">
-                                                    <Code2 className="mr-2 h-4 w-4" />
-                                                    Copy Donate Embed
-                                                </DropdownMenuItem>
-                                            )}
+                                            <DropdownMenuItem onClick={handleCopyDonateEmbed} className="hover:bg-foreground/10 focus:bg-foreground/10 cursor-pointer">
+                                                <Code2 className="mr-2 h-4 w-4" />
+                                                Copy Donate Embed
+                                            </DropdownMenuItem>
                                             <DropdownMenuSeparator className="bg-foreground/10" />
                                             <DropdownMenuItem onClick={() => setQrModalOpen(true)} className="hover:bg-foreground/10 focus:bg-foreground/10 cursor-pointer">
                                                 <QrCode className="mr-2 h-4 w-4" />
