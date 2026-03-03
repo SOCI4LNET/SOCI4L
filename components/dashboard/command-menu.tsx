c6401eeb54ba6d067efc51c45fe80e1f652dc6e7
xBrokkr <merickalkanp@gmail.com>
2026-03-03 03:15:54 +0300

    ✔  CTRL+K shortcurt added to dashboard

--- components/dashboard/command-menu.tsx
+++ components/dashboard/command-menu.tsx
@@ -50,11 +50,11 @@ export function DashboardCommandMenu() {
 
     return (
         <Dialog open={open} onOpenChange={setOpen}>
-            <DialogContent className="overflow-hidden p-0 shadow-2xl rounded-xl border border-white/10 bg-background/30 backdrop-blur-xl supports-[backdrop-filter]:bg-background/20 dark:bg-black/30 dark:supports-[backdrop-filter]:bg-black/20">
+            <DialogContent className="overflow-hidden p-0 shadow-lg">
                 <DialogTitle className="sr-only">Command Menu</DialogTitle>
-                <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5 bg-transparent">
-                    <CommandInput placeholder="Bir komut yazın veya arama yapın..." className="bg-transparent border-none focus:ring-0" />
-                    <CommandList className="bg-transparent">
+                <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+                    <CommandInput placeholder="Bir komut yazın veya arama yapın..." />
+                    <CommandList>
                         <CommandEmpty>Sonuç bulunamadı.</CommandEmpty>
 
                         <CommandGroup heading="Kısayollar">
@@ -72,7 +72,7 @@ export function DashboardCommandMenu() {
                             </CommandItem>
                         </CommandGroup>
 
-                        <CommandSeparator className="bg-white/10 dark:bg-white/10" />
+                        <CommandSeparator />
 
                         <CommandGroup heading="Paneller">
                             <CommandItem onSelect={() => runCommand(() => router.push(`${pathname}?tab=overview`))}>
